79da5069738ed1ecaa25f0ed4bdf8bf2ec61d0d7
BrandonArgel <brandargel@gmail.com>
2022-03-11 22:02:55 -0600

    :zap: [ADD] React lazy

--- src/routes/App.tsx
+++ src/routes/App.tsx
@@ -1,17 +1,17 @@
-import { Suspense } from "react";
+import * as React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import Home from "pages/index";
-import Development from "pages/Development";
-import Confort from "pages/Development/Confort";
 
 import { Layout, PostLayout } from "components/Layout";
 import Loader from "components/loader";
 
+const Home = React.lazy(() => import("pages/index"));
+const Development = React.lazy(() => import("pages/Development"));
+const Confort = React.lazy(() => import("pages/Development/Confort"));
+
 const App = () => {
 	return (
 		<BrowserRouter>
-			<Suspense fallback={<Loader />}>
+			<React.Suspense fallback={<Loader />}>
 				<Routes>
 					<Route path="/" element={<Layout />}>
 						<Route index element={<Home />} />
@@ -21,7 +21,7 @@ const App = () => {
 						<Route path="como-salir-de-tu-zona-de-confort" element={<Confort />} />
 					</Route>
 				</Routes>
-			</Suspense>
+			</React.Suspense>
 		</BrowserRouter>
 	);
 };
