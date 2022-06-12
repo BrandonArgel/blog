c7550d8acc3482974c2af3c045ce5f5d3f4713f0
BrandonArgel <brandargel@gmail.com>
2022-06-12 09:13:18 -0500

    :bug: Flip arrow of aside when it is open

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 import { Title } from "components";
 
-import { ArrowRight } from "assets/icons/arrrows";
+import { ArrowRight, ArrowLeft } from "assets/icons/arrrows";
 
 import styles from "./index.module.scss";
 
@@ -18,14 +18,22 @@ interface Props {
 
 const Aside: React.FC<Props> = ({ module, topics }) => {
 	const [isOpen, setIsOpen] = React.useState(false);
-	const buttonRef = React.useRef<HTMLButtonElement>(null) as React.MutableRefObject<HTMLButtonElement>;
+	const buttonRef = React.useRef<HTMLButtonElement>(
+		null
+	) as React.MutableRefObject<HTMLButtonElement>;
 
 	return (
 		<>
 			<aside className={`${styles.aside} ${isOpen ? styles.open : ""}`} tabIndex={-1}>
-				<header className={styles.header}>
-					<button ref={buttonRef} className={`${styles.button} ${isOpen ? styles.active : ""}`} onClick={() => setIsOpen(!isOpen)} aria-label={!isOpen ? "Abrir menú de temas" : "Cerrar menú de temas"}>
-						<ArrowRight />
+				<header className={styles.aside__header}>
+					<button
+						ref={buttonRef}
+						className={styles.aside__header_button}
+						onClick={() => setIsOpen(!isOpen)}
+						aria-label={!isOpen ? "Abrir menú de temas" : "Cerrar menú de temas"}
+						type="button"
+					>
+						{!isOpen ? <ArrowRight /> : <ArrowLeft />}
 					</button>
 					<Link to={`/${module.route}`} tabIndex={isOpen ? 0 : -1} aria-hidden={!isOpen}>
 						<Title h={2}>
@@ -35,10 +43,14 @@ const Aside: React.FC<Props> = ({ module, topics }) => {
 				</header>
 				<section id="summary" className={styles.topics}>
 					{topics.map((topic, i) => (
-						<a href={`#${i + 1}`} key={i} onClick={() => {
-							buttonRef.current.focus();
-							setIsOpen(false)
-						}}>
+						<a
+							href={`#${i + 1}`}
+							key={i}
+							onClick={() => {
+								buttonRef.current.focus();
+								setIsOpen(false);
+							}}
+						>
 							<div className={styles.item}>
 								<span className={styles.number}>{i + 1}</span>
 								<div className={styles.name}>
